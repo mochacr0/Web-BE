5a97b28840f2e86c19b7a3ae4b022c752e253115
mochacr0 <1951120093@sv.ut.edu.vn>
2022-11-07 14:37:51 +0700

    update: order model, controller, route (haven't tested)

---
+++ routes/order.route.js
@@ -0,0 +1,51 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import { auth, protect } from "../middlewares/auth.middleware.js";
+import orderController from "../controllers/order.controller.js";
+
+const orderRouter = express.Router();
+
+orderRouter.post(
+  "/",
+  protect,
+  auth("user"),
+  asyncHandler(orderController.placeOrder)
+);
+orderRouter.get(
+  "/all",
+  protect,
+  auth("admin"),
+  asyncHandler(orderController.getOrders)
+);
+orderRouter.get(
+  "/:id",
+  protect,
+  auth("user", "admin"),
+  asyncHandler(orderController.getOrderById)
+);
+orderRouter.get(
+  "/",
+  protect,
+  auth("user"),
+  asyncHandler(orderController.getOrdersByUserId)
+);
+orderRouter.patch(
+  "/:id",
+  protect,
+  auth("user", "admin"),
+  asyncHandler(orderController.updateOrderStatus)
+);
+orderRouter.patch(
+  "/:id/cancel",
+  protect,
+  auth("user", "admin"),
+  asyncHandler(orderController.cancelOrder)
+);
+orderRouter.post(
+  "/:id/orderItem/:orderItemId/product/:productId",
+  protect,
+  auth("user"),
+  asyncHandler(orderController.reviewProductByOrderItemId)
+);
+
+export default orderRouter;
